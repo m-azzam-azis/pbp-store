3ffde41c2769b4d0d23c860feaa37214197a3dc8
m.azzam <m.azzam.azis@gmail.com>
2024-09-04 12:15:31 +0700

    refactor: ganti models dan migrate

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class MoodEntry(models.Model):
-    name = models.CharField()
+class ShopEntry(models.Model):
+    name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
