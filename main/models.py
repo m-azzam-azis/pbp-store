58540f510c255257c99a7464deb53db7b67ac5dc
m.azzam <m.azzam.azis@gmail.com>
2024-09-04 13:41:52 +0700

    feat: update models to include more attributes

--- main/models.py
+++ main/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class ShopEntry(models.Model):
+    # wajib
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    
+    # optional
+    sold = models.IntegerField(default=0)
+    rating = models.FloatField(default=None)
+    
+    @property
+    def is_top_seller(self):
+        return self.sold > 1000 and self.rating > 4.0
