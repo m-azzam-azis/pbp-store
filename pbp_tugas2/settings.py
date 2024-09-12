fb7637557adb76fff9f1923a84973b0acce1acb5
m.azzam <m.azzam.azis@gmail.com>
2024-09-12 10:47:53 +0700

    feat: done tugas3

--- pbp_tugas2/settings.py
+++ pbp_tugas2/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-azzam31-tugas2.pbp.cs.ui.ac.id", "https://muhammad-azzam31-tugas2.pbp.cs.ui.ac.id"]
