665878d2f3b8d6a2b75a529b1926f277f98fbe9b
m.azzam <m.azzam.azis@gmail.com>
2024-09-12 11:08:24 +0700

    fix: replace all mood into shop

--- main/models.py
+++ main/models.py
@@ -1,7 +1,12 @@
+import uuid  # tambahkan baris ini di paling atas
 from django.db import models
+
 
 # Create your models here.
 class ShopEntry(models.Model):
+    # id
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
+
     # wajib
     name = models.CharField(max_length=255)
     price = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -35,9 +35,9 @@ def show_json(request):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = ShopEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = ShopEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data),content_type="application/json")
