851e168616af42c2faed4ac3fa67deca0aa3c784
m.azzam <m.azzam.azis@gmail.com>
2024-10-07 17:31:09 +0700

    feat: checklist tugas 6

--- main/views.py
+++ main/views.py
@@ -17,9 +17,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
-
-
-
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
